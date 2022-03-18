e545703bf373bdd5893273b80cfa2e7edfd75460
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-03-18 14:26:41 +0300

    review fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const fixButton = document.querySelectorAll('.button');
 function buttonChange(evt){
-    evt.target.closest('.button__text').textContent = 'Круто, спасибо за доверие!';
+    evt.target.closest('.button').textContent = 'Круто, спасибо за доверие!';
   }
 fixButton.forEach(item => item.addEventListener('click', buttonChange));
